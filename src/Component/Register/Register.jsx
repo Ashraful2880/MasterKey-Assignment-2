7f472a8488fa6f298a66bde4a1ed4a6262fc3d1b
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-31 13:37:48 +0600

    Login & Register Page Design Done

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -4,14 +4,14 @@ import logo from "../../Images/Main-Logo.png";
 
 const Register = () => {
     return (
-        <div className="bg-style">
+        <div className="register-style">
             <div className="container mx-auto">
                 <div className="w-[50%] rounded-md py-10 mx-auto">
-                    <p className="text-orange-600 text-3xl mb-4 font-semibold">Please Signup</p>
                     <img className="w-20 mx-auto" src={logo} alt="logo" />
-                    <p className="text-2xl text-white font-semibold mb-6">
+                    <p className="text-2xl text-white font-semibold mb-2">
                         <span className="text-orange-600">M</span>r <span className="text-orange-600">M</span>odernistic
                     </p>
+                    <p className="text-white text-3xl mb-4 font-semibold">Please Signup</p>
                     <form className="w-[40%] mx-auto">
                         <div className="mb-4">
                             <p className="text-lg text-white font-semibold text-left block">Your Name</p>
@@ -40,20 +40,13 @@ const Register = () => {
                             Signup
                         </button>
                     </form>
-                    <p className="text-white text-xl mt-5 ">Already Register?
+                    <p className="text-white text-lg mt-5 ">Already Register?
                         <Link
                             className="ml-4 text-orange-500"
                             to="/login">
                             Sign in here
                         </Link>
                     </p>
-                    <button
-                        className="w-[40%] mx-auto flex justify-evenly items-center my-6 px-10 py-2 border border-orange-600 rounded-md duration-200 bg-white hover:bg-orange-600 hover:text-white text-lg font-bold">
-                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/sign_in_2-svg2.svg" alt="google" />
-                        <p>
-                            Continue with Google
-                        </p>
-                    </button>
                 </div>
             </div>
         </div >
